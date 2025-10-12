9e5c113f0de242536fe092eb01fbfa849e1e066a
Valera Tsyplakov <webvalera96@yandex.ru>
2025-10-12 23:07:47 +0300

    - fix tests after adding chi

--- internal/handler/update_test.go
+++ internal/handler/update_test.go
@@ -1,12 +1,22 @@
 package handler
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 )
+
+func AddChiURLParams(r *http.Request, params map[string]string) *http.Request {
+	ctx := chi.NewRouteContext()
+	for k, v := range params {
+		ctx.URLParams.Add(k, v)
+	}
+	return r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, ctx))
+}
 
 func TestMetricUpdate(t *testing.T) {
 
@@ -14,40 +24,47 @@ func TestMetricUpdate(t *testing.T) {
 		StatusCode int
 	}
 	tests := []struct {
-		name    string
-		request string
+		name      string
+		request   string
+		chiParams map[string]string
 		want
 	}{
 		{
 			name:    "successfull gauge metric update #1",
 			request: "/update/gauge/HeapObjects/3522.0",
-			want:    want{StatusCode: 200},
+			chiParams: map[string]string{
+				"metricType":  "gauge",
+				"metricName":  "HeapObjects",
+				"metricValue": "3522.0",
+			},
+			want: want{StatusCode: 200},
 		},
 		{
 			name:    "successfull counter metric update #2",
 			request: "/update/counter/PollCount/2",
-			want:    want{StatusCode: 200},
+			chiParams: map[string]string{
+				"metricType":  "counter",
+				"metricName":  "PollCount",
+				"metricValue": "2",
+			},
+			want: want{StatusCode: 200},
 		},
 		{
-			name:    "unsuccessfull bad request (wrong url) #3",
-			request: "/\\/\\///",
-			want:    want{StatusCode: 400},
-		},
-		{
-			name:    "unsuccessfull no metric in url #4",
+			name:    "unsuccessfull no metric in url #3",
 			request: "/update/gauge/3522.0",
-			want:    want{StatusCode: 404},
-		},
-		{
-			name:    "unsuccessfull wrong metric data  #5",
-			request: "/update/gauge/HeapObjects/3543.0/34534.0",
-			want:    want{StatusCode: 400},
+			chiParams: map[string]string{
+				"metricType":  "gauge",
+				"metricName":  "",
+				"metricValue": "3522.0",
+			},
+			want: want{StatusCode: 404},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			request := httptest.NewRequest(http.MethodPost, tt.request, nil)
+			request = AddChiURLParams(request, tt.chiParams)
 			w := httptest.NewRecorder()
 			h := http.HandlerFunc(Update)
 			h(w, request)
