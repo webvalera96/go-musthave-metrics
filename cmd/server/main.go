dc6e95a57b348c39e2f474559538da5e11c2dfb1
Valera Tsyplakov <webvalera96@yandex.ru>
2025-10-13 00:24:29 +0300

    try to fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,7 @@ var flagRunAddr string
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
