a8da629c4f8b67664892b3f8caa802c3dd19e20d
Valera Tsyplakov <webvalera96@yandex.ru>
2025-10-12 18:02:24 +0300

    - write one test for successfull update value

---
+++ internal/handler/update_test.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetricUpdate(t *testing.T) {
+
+	type want struct {
+		StatusCode int
+	}
+	tests := []struct {
+		name    string
+		request string
+		want
+	}{
+		{
+			name:    "successfull gauge metric update #1",
+			request: "/update/gauge/HeapObjects/3522.0",
+			want:    want{StatusCode: 200},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, tt.request, nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(Update)
+			h(w, request)
+
+			result := w.Result()
+			assert.Equal(t, tt.want.StatusCode, result.StatusCode)
+		})
+	}
+
+}
