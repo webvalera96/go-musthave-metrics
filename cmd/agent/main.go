7f4a8c15ed39a0ac7a6b9b25d9c47dd222b302f8
Валерий Цыплаков <webvalera96@gmail.com>
2025-11-05 01:03:19 +0300

    - refactor agent flags and metrics to internal packages for agent (internal/agent)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"errors"
-	"flag"
-	"fmt"
-	"log"
-	"math/rand"
 	"net/http"
-	"os"
-	"reflect"
 	"runtime"
-	"slices"
-	"strconv"
-	"sync"
 	"time"
 
-	models "github.com/webvalera96/go-musthave-metrics/internal/model"
+	"github.com/webvalera96/go-musthave-metrics/internal/agent/flags"
+	"github.com/webvalera96/go-musthave-metrics/internal/agent/metrics"
 	"go.uber.org/fx"
 )
 
@@ -26,235 +15,14 @@ import (
 // const reportInterval = 10
 // const baseURL = "localhost:8080" //TODO: move to configuration of agent
 
-var MemoryMetrics = []string{
-	"Alloc",
-	"BuckHashSys",
-	"Frees",
-	"GCCPUFraction",
-	"GCSys",
-	"HeapAlloc",
-	"HeapIdle",
-	"HeapInuse",
-	"HeapObjects",
-	"HeapReleased",
-	"HeapSys",
-	"LastGC",
-	"Lookups",
-	"MCacheInuse",
-	"MCacheSys",
-	"MSpanInuse",
-	"MSpanSys",
-	"Mallocs",
-	"NextGC",
-	"NumForcedGC",
-	"NumGC",
-	"OtherSys",
-	"PauseTotalNs",
-	"StackInuse",
-	"StackSys",
-	"Sys",
-	"TotalAlloc",
-}
-
-type RuntimeMetrics struct {
-	mu                   sync.Mutex
-	RuntimeMemoryMetrics runtime.MemStats
-	PollCount            uint64
-	RandomValue          float64
-}
-
-func (rm *RuntimeMetrics) Set(m runtime.MemStats) {
-	rm.mu.Lock()
-	defer rm.mu.Unlock()
-	rm.RuntimeMemoryMetrics = m
-	rm.PollCount++
-	rm.RandomValue = rand.Float64()
-}
-
-func (rm *RuntimeMetrics) Get() runtime.MemStats {
-	rm.mu.Lock()
-	defer rm.mu.Unlock()
-	return rm.RuntimeMemoryMetrics
-}
-
-func sendMetric(client *http.Client, baseURL string, metricType string, metricName string, metricValue string) error {
-	requestURL := fmt.Sprintf("http://%s/update/", baseURL)
-
-	var metric models.Metrics
-
-	if metricType == models.Counter {
-		delta, err := strconv.ParseInt(metricValue, 10, 64)
-		if err != nil {
-			return err
-		}
-
-		metric = models.Metrics{
-			ID:    metricName,
-			MType: models.Counter,
-			Delta: &delta,
-		}
-	} else if metricType == models.Gauge {
-		value, err := strconv.ParseFloat(metricValue, 64)
-		if err != nil {
-			return err
-		}
-
-		metric = models.Metrics{
-			ID:    metricName,
-			MType: models.Gauge,
-			Value: &value,
-		}
-	} else {
-		return errors.New("wrong type of metric")
-	}
-
-	body, err := json.Marshal(metric)
-	if err != nil {
-		return err
-	}
-
-	request, err := http.NewRequest(http.MethodPost, requestURL, bytes.NewBuffer(body))
-	if err != nil {
-		return err
-	}
-	request.Header.Set("Content-Type", "application/json")
-
-	response, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("[%s] unable to send metric: %s, with value %s", time.Now().Format(time.RFC3339), metricName, metricValue)
-	} else {
-		fmt.Printf("[%s] %s is ok\n", time.Now().Format(time.RFC3339), requestURL)
-	}
-
-	return nil
-}
-
-func sendMetricOLD(client *http.Client, baseURL string, metricType string, metricName string, metricValue string) error {
-	requestURL := fmt.Sprintf("http://%s/update/%s/%s/%s", baseURL, metricType, metricName, metricValue)
-	request, err := http.NewRequest(http.MethodPost, requestURL, nil)
-	if err != nil {
-		return err
-	}
-	request.Header.Set("Content-Type", "text/plain")
-	response, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("[%s] unable to send metric: %s, with value %s", time.Now().Format(time.RFC3339), metricName, metricValue)
-	} else {
-		fmt.Printf("[%s] %s is ok\n", time.Now().Format(time.RFC3339), requestURL)
-	}
-
-	return nil
-}
-
-func (rm *RuntimeMetrics) SendToMetricsStorage(client *http.Client) error {
-	// send memory metrics
-	value := reflect.ValueOf(rm.RuntimeMemoryMetrics)
-	typ := value.Type()
-	for i := 0; i < value.NumField(); i++ {
-		metricName := typ.Field(i).Name
-		if slices.Contains(MemoryMetrics, metricName) {
-
-			fieldValue := value.Field(i).Interface()
-			fieldType := reflect.TypeOf(fieldValue)
-			var metricValue float64
-			if fieldType == reflect.TypeOf(uint64(0)) {
-				metricValue = float64(fieldValue.(uint64))
-			} else if fieldType == reflect.TypeOf(uint32(0)) {
-				metricValue = float64(fieldValue.(uint32))
-			} else if fieldType == reflect.TypeOf(float64(0)) {
-				metricValue = fieldValue.(float64)
-			} else {
-				fmt.Printf("unknown type of metrics: %v\n", fieldType)
-				continue
-			}
-
-			// send gauge metrics of agent mem stats
-			err := sendMetric(client, flagMetricsServer, models.Gauge, metricName, strconv.FormatFloat(metricValue, 'f', 3, 64))
-			if err != nil {
-				return err
-			}
-
-		}
-	}
-	// send poll counts
-	err := sendMetric(client, flagMetricsServer, models.Counter, "PollCount", strconv.FormatUint(rm.PollCount, 10))
-	if err != nil {
-		return err
-	}
-
-	// send random value
-	err = sendMetric(client, flagMetricsServer, models.Gauge, "RandomValue", strconv.FormatFloat(rm.RandomValue, 'f', 3, 64))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-var flagMetricsServer string
-var flagPollInterval int
-var flagReportPollInterval int
-
-const (
-	EnvAddress        = "ADDRESS"
-	EnvReportInterval = "REPORT_INTERVAL"
-	EnvPollInterval   = "POLL_INTERVAL"
-)
-
-func parseFlags() {
-	// Parse metrics server address
-	var exist bool
-	flagMetricsServer, exist = os.LookupEnv(EnvAddress)
-	if !exist {
-		flag.StringVar(&flagMetricsServer, "a", "localhost:8080", "address and port of metric server")
-	}
-
-	// Parse poll interval
-	var pollInterval string
-	pollInterval, exist = os.LookupEnv(EnvPollInterval)
-	if exist {
-		var err error
-		flagPollInterval, err = strconv.Atoi(pollInterval)
-
-		if err != nil {
-			log.Fatal("unable to parse POLL_INTERVAL env value")
-		}
-	} else {
-		flag.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
-	}
-
-	// Parse flagreport interval
-	var reportPollInterval string
-	reportPollInterval, exist = os.LookupEnv(EnvReportInterval)
-	if exist {
-		var err error
-		flagReportPollInterval, err = strconv.Atoi(reportPollInterval)
-
-		if err != nil {
-			log.Fatal("unable to parse REPORT_INTERVAL env value")
-		}
-	} else {
-		flag.IntVar(&flagReportPollInterval, "r", 10, "report interval in seconds")
-	}
-
-	flag.Parse()
-}
-
-func MetricsUpdater(lc fx.Lifecycle) *RuntimeMetrics {
-	rm := RuntimeMetrics{}
+func MetricsUpdater(lc fx.Lifecycle) *metrics.RuntimeMetrics {
+	rm := metrics.RuntimeMetrics{}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			go func() {
 				for {
 					// Pause between metrics gathering
-					time.Sleep(time.Second * time.Duration(flagPollInterval))
+					time.Sleep(time.Second * time.Duration(flags.FlagPollInterval))
 
 					// update current runtimeMetrics
 					var m runtime.MemStats
@@ -265,20 +33,20 @@ func MetricsUpdater(lc fx.Lifecycle) *RuntimeMetrics {
 			return nil
 		},
 		OnStop: func(ctx context.Context) error {
-			rm = RuntimeMetrics{}
+			rm = metrics.RuntimeMetrics{}
 			return nil
 		},
 	})
 	return &rm
 }
 
-func MetricsSender(lc fx.Lifecycle, runtimeMetrics *RuntimeMetrics) *http.Client {
+func MetricsSender(lc fx.Lifecycle, runtimeMetrics *metrics.RuntimeMetrics) *http.Client {
 	client := http.Client{}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			go func() {
 				for {
-					time.Sleep(time.Second * time.Duration(flagReportPollInterval))
+					time.Sleep(time.Second * time.Duration(flags.FlagReportPollInterval))
 					runtimeMetrics.SendToMetricsStorage(&client)
 				}
 			}()
@@ -294,7 +62,7 @@ func MetricsSender(lc fx.Lifecycle, runtimeMetrics *RuntimeMetrics) *http.Client
 
 func main() {
 
-	parseFlags()
+	flags.ParseFlags()
 
 	fx.New(
 		fx.Provide(
