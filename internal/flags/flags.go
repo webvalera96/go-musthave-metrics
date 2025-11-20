ba97c254a71d331f40a24c027b99915637b1a83a
Валерий Цыплаков <webvalera96@gmail.com>
2025-11-20 23:10:20 +0300

    - Refactor Taskfile to enable multiple test iterations and enhance flag parsing in - fix flags parsing

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -26,31 +26,33 @@ func ParseFlags() {
 
 	var runAddr string
 	runAddr, exist = os.LookupEnv(EnvAddress)
+
+	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
+
 	if exist {
 		FlagRunAddr = runAddr
-	} else {
-		flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	}
 
 	var storeInterval string
 	storeInterval, exist = os.LookupEnv(EnvStoreInterval)
+
+	flag.IntVar(&FlagStoreInterval, "i", 300, "time interval to save data on disk")
+
 	if exist {
 		var err error
 		FlagStoreInterval, err = strconv.Atoi(storeInterval)
 		if err != nil {
 			log.Fatalf("unable to parse %s env value", EnvStoreInterval)
 		}
-	} else {
-		flag.IntVar(&FlagStoreInterval, "i", 300, "time interval to save data on disk")
 	}
 
 	var fileStoragePath string
 	fileStoragePath, exist = os.LookupEnv(EnvFileStoragePath)
 
+	flag.StringVar(&FlagStoragePath, "f", "", "path to saved data in file")
+
 	if exist {
 		FlagStoragePath = fileStoragePath
-	} else {
-		flag.StringVar(&FlagStoragePath, "f", "", "path to saved data in file")
 	}
 
 	var restore string
@@ -64,16 +66,15 @@ func ParseFlags() {
 		if err != nil {
 			log.Fatalf("unabble to parse %s env value", EnvRestore)
 		}
-	} else {
-		flag.BoolVar(&FlagRestore, "r", false, "restore from file")
 	}
 
 	var databaseDSN string
 	databaseDSN, exist = os.LookupEnv(EnvDatabaseDSN)
+
+	flag.StringVar(&FlagDatabaseDSN, "d", "", "database DSN")
+
 	if exist {
 		FlagDatabaseDSN = databaseDSN
-	} else {
-		flag.StringVar(&FlagDatabaseDSN, "d", "", "database DSN")
 	}
 
 	flag.Parse()
